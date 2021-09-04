e8111a219881dc22258131c842206b599453e7d7
djreyab <djreya.boyd@gmail.com>
2021-09-04 00:43:47 -0400

    Created layout for singly linked list class

---
+++ LinkedList/Singly.py
@@ -0,0 +1,100 @@
+class SinglyLinked():
+    def __init__(self, head, tail, next) -> None:
+        self.head = head
+        self.tail = tail
+        self.next = next
+
+
+    def push(self, val):
+        '''
+        Description:
+            Appends a user specified value to the end of the linked list.
+            Increases length.
+
+        Arguments:
+            val: user specified value
+
+        Return:
+            linked list object
+        '''
+
+
+        pass
+
+    def pop(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+
+        '''
+        
+        pass
+
+    def shift(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+        
+        '''
+        pass
+
+    def unshift(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+        
+        '''
+        pass
+
+    def get(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+        
+        '''
+        pass
+
+    def set(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+        
+        '''
+        pass
+
+    def insert(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+        
+        '''
+        pass
+
+    def unshift(self):
+        '''
+        Description:
+
+        Arguments:
+
+        Return:
+        
+        '''
+        pass
---
+++ Problem Sovling/README.md
@@ -0,0 +1,13 @@
+# Colt-Steele-Python-Aglorithms-DataStructures
+
+Data Structures and Algorithms is a udemy course taught by Colt Steele.
+The course is amazing; the code is javascript. As my bread and butter and 
+one true love for the moment is Python, I'd like to code along to this class
+with Python. 
+
+If you see an answer that can be simplified, please feel free to comment or contribute.
+The best way to become the best is to be evaluated by the better.
+
+TOC
+
+1 Problem Solving Patterns
